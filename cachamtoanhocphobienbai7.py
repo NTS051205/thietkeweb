f5ace37558dabb94a5c7435585ef71fcb56a6ff8
ntsonn0512 <101138727+NTS051205@users.noreply.github.com>
2025-04-10 23:33:29 +0700

    Add files via upload

---
+++ cachamtoanhocphobienbai7.py
@@ -0,0 +1,48 @@
+from math import *
+#cách 2
+print(sqrt(36))
+#cách 1 
+#import math
+#print(help(math))
+#sqrt : square root
+#print(math.sqrt(36))
+#số xuất ra là số thực 
+
+#isqrt: integer square root
+#(tính căn bậc 2 ra nma chỉ lấy phần nguyên)
+print(isqrt(39))
+print(isqrt(4))
+
+#pow:power : lũy thừa
+print(pow(2,10))
+#trả về số float
+
+#ceil:ceiling(làm tròn lên)
+print(ceil(2.3))
+print(ceil(7.8))
+
+#floor (làm tròn xuống
+print(floor(2.3))
+print(floor(3.5))
+
+#factorial : giai thừa
+print(factorial(100))
+
+#gcd(a,b): greatest common divisor
+print(gcd(100,450))
+#tìm ước chung lớn nhất
+
+#comb(n,k): tính tổ hợp chập k của n
+print(comb(10,2))
+
+#perm
+print(perm(10,2))
+
+print(fabs(-100.2349))
+#tính trị tuyệt đối của số thực
+#built-in function(làm trên lên hoặc xuống như bth)
+print(round(2093.3393))
+print(max(1, 2, 3, 4, 5))
+#abs min max sum(abs là trị tuyệt đối)
+
+
